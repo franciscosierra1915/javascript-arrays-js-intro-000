8c4fef565fb0bfb8f03acd6dc3e06be13389b655
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-24 21:39:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  
+  array.push(element)
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.push(element)
+  
 }
